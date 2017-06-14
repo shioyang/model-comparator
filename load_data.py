bdd233b874e762e3f1e5975150b18cfb803cf7b1
shioyang <shioyang4321@gmail.com>
2017-06-15 01:04:21 +0900

    Refactoring

--- load_data.py
+++ load_data.py
@@ -1,8 +1,7 @@
-import os
 import numpy as np
 from keras.preprocessing.image import load_img, list_pictures, img_to_array
 
-def load_img_asarray(dir, ext):
+def load_img_asarray(data_dir, ext):
     '''
     Import images from the directory as array, and append it to container array, X:
       2 pictures as array
@@ -42,7 +41,7 @@ def load_img_asarray(dir, ext):
             [  65.,   94.,   72.]]], dtype=float32)]
     '''
     X = []
-    for pic in list_pictures(dir, ext):
+    for pic in list_pictures(data_dir, ext):
         img = img_to_array( load_img(pic) )
         X.append(img)
     
