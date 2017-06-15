8a40fcdf25fca0bd6eb066d88c00ae9bbe079122
shioyang <shioyang4321@gmail.com>
2017-06-15 18:21:35 +0900

    Update modification

--- modify_meta_data.py
+++ modify_meta_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+from os.path import isfile
 from scipy.io import loadmat, savemat
 from keras.preprocessing.image import load_img, list_pictures, img_to_array
 
@@ -26,21 +27,26 @@ def load_img_label_asarray(data_dir, ext):
 
     for name, full_path, gender, face_score, second_face_score in zip(name_list, full_path_list, gender_list, face_location_list, second_face_score_list):
         if gender != 0 and gender != 1:
+            print('Pass gender NaN:', full_path[0])
             continue
-        if np.isnan(second_face_score):
+        if not np.isnan(second_face_score):
+            print('Pass second face score not NaN:', full_path[0])
+            continue
+        if not isfile(data_dir + '/' + full_path[0].strip()):
+            print('Pass not exist:', full_path[0])
             continue
         # if face_score > 1.0:
         #     continue
-        full_paths.append(full_path)
-        genders.append(gender)
+        full_paths.append( full_path[0] )
+        genders.append( gender )
         # print(name, full_path, gender, face_score, second_face_score)
 
-    savemat('modified_wiki.mat', { "full_path": full_paths, "gender": genders })
+    savemat('modified_wiki.mat', { "full_path": np.array(full_paths), "gender": np.array(genders) })
 
 
 # load_label('data2/wiki.mat')
-load_img_label_asarray('data2', 'jpg')
+load_img_label_asarray('data', 'jpg')
 
-# X = load_img_asarray('data2', 'jpg')
+# X = load_img_asarray('data', 'jpg')
 # print(X)
 
