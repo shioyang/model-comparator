a7b014b1cf24d919561e1712c60b8d430f5866da
shioyang <shioyang4321@gmail.com>
2017-06-16 15:41:56 +0900

    Refactoring

--- ml/predict.py
+++ ml/predict.py
@@ -1,9 +1,12 @@
 import numpy as np
+import argparse
 from keras.models import load_model
 from keras.preprocessing.image import load_img, img_to_array
-import argparse
+from keras.backend import tensorflow_backend
 
+### Configuration
 img_rows, img_cols = 128, 128
+model_dir = 'trained_model.h5'
 
 
 def get_args():
@@ -15,14 +18,22 @@ def get_args():
 args = get_args()
 img_path = args.input
 
+
+print('')
+print('### Loading img:', img_path)
 img = load_img(img_path, target_size=(img_rows, img_cols))
 img_arr = img_to_array(img)
 img_array = np.array([img_arr])
 
 
-model = load_model('trained_model.h5')
+print('')
+print('### Loading model:', model_dir)
+model = load_model(model_dir)
 model.summary()
 
+
+print('')
+print('### Prediction')
 print('********************')
 print('*   0 for female   *')
 print('*   1 for male     *')
@@ -32,4 +43,3 @@ for res in results:
     print('Result:')
     print('   Female:', res[0] * 100)
     print('   Male  :', res[1] * 100)
-
