328a77e371968ad4eed7f2192993778db122257c
shioyang <shioyang4321@gmail.com>
2017-06-28 16:03:14 +0900

    Use full path to access image and model files

--- src/ml/predict.py
+++ src/ml/predict.py
@@ -1,43 +1,49 @@
 import numpy as np
 import argparse
+import os
 from keras.models import load_model
 from keras.preprocessing.image import load_img, img_to_array
 
 ### Ignore tf warning: 'The TensorFlow library wasn't compiled to use...'
 ### Ref: https://github.com/tensorflow/tensorflow/issues/7778
-# import os
 # os.environ['TF_CPP_MIN_LOG_LEVEL']='2'  # Ignore tf warning: 'The TensorFlow library wasn't compiled to use...'
 
 ### Configuration
 img_rows, img_cols = 128, 128
-model_dir = 'trained_model.h5'
+model_path = 'trained_model.h5'
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='Predict gender which the person\'s in the image is.')
-    parser.add_argument('--input', '-i', type=str, required=True, help='path to input an image')
+    parser.add_argument('--input', '-i', type=str, required=True, help='path to input an image from the script path')
     return parser.parse_args()
 
 
 args = get_args()
 img_path = args.input
 
+script_abs_path = os.path.abspath(os.path.dirname(__file__))
 
+
+### Load Image for Prediction
 print('')
 print('### Loading img:', img_path)
-img = load_img(img_path, target_size=(img_rows, img_cols))
+img_full_path = script_abs_path + '/' + img_path
+img = load_img(img_full_path, target_size=(img_rows, img_cols))
 img_arr = img_to_array(img)
 img_array = np.array([img_arr])
 img_array = img_array.astype('float32')
 img_array /= 255
 
-
+### Load Trained Model
 print('')
-print('### Loading model:', model_dir)
-model = load_model(model_dir)
+print('### Loading model:', model_path)
+model_full_path = script_abs_path + '/' + model_path
+model = load_model(model_full_path)
 model.summary()
 
 
+### Prediction
 print('')
 print('### Prediction')
 print('********************')
