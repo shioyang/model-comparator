e172fdc63f35db9199320cf875e74485f0d50c6a
shioyang <shioyang4321@gmail.com>
2017-07-21 14:48:20 +0900

    Call py_server from server

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ router.get('/', function(req, res, next) {
 
 var cmd = require('node-cmd')
 
+// http://localhost:3000/users/ml
 router.get('/ml', function(req, res, next) {
   // Note: Activate TensorFlow before execute server.
   var pyCommand = 'cd ./src/ml && python predict.py -i data_prediction/2_pred.jpg'
@@ -22,6 +23,68 @@ router.get('/ml', function(req, res, next) {
       console.log('Error: ' + pyCommand)
       console.log('     : ' + stderr)
       res.send('Error: ' + pyCommand + '\n' + stderr)
+    }
+  })
+})
+
+
+var pysh = require('python-shell')
+
+// http://localhost:3000/users/ml2
+router.get('/ml2', function(req, res, next) {
+  // Note: Activate TensorFlow before execute server.
+  var pyCommand = 'predict.py'
+  var options = {
+    scriptPath: 'src/ml',
+    args: ['-i','data_prediction/2_pred.jpg']
+  }
+
+  pysh.run(pyCommand, options, function(err, results){
+    if(err){
+      console.log('Error: ' + pyCommand)
+      console.log('     : ' + err)
+      console.log('?????: %j', results)
+      res.send('Error: ' + pyCommand + '\n' + err)
+    }else{
+      // success
+      console.log('Success: %j', results)
+      res.send('Success: ' + results)
+    }
+  })
+  
+})
+
+
+var request = require('request')
+
+// http://localhost:3000/users/callpy
+router.get('/callpy', function(req, res, next){
+  request.get('http://localhost:5000/ml', function(error, response, body){
+    res.send('body: ' + body)
+  })
+})
+
+// http://localhost:3000/users/predict
+router.get('/predict', function(req, res, next){
+  let param = {
+    url:  'http://localhost:5000/ml',
+    formData: {
+      file_path: 'data_prediction/2_pred.jpg' //temp
+    }
+  }
+  request.post(param, function(error, response, body){
+    if(error){
+      res.send('Error: ' + error)
+    }else{
+      // res.send('res statusCode: ' + response.statusCode)
+      if(response.statusCode === 200){
+        if(body)
+          res.send(body)
+        else
+          res.send('body is empty.')
+      }else{
+        res.send('status code: ' + response.statusCode)
+      }
     }
   })
 })
--- src/ml/py_server.py
+++ src/ml/py_server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,7 +7,18 @@ def hello():
     return '{ "text": "Hello World!" }'
 
 
-@app.route("/ml")
+@app.route("/ml", methods=['GET', 'POST'])
 def route_ml():
-    s1 = 'ml'
-    return '{ "test": "' + s1 + '" }'
+    file_path = ''
+    print(request.method)
+    print(request.form['file_path'])
+    if request.method == 'POST':
+        file_path = request.form['file_path']
+    if file_path != '':
+        return predict_file(file_path)
+    else:
+        return 'File path not provided.'
+
+def predict_file(file_path):
+    print('predict')
+    return 'predicted: ' + file_path
