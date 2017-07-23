09b7944c61d6a846ca6f04d6ecef54f4b3fe5b88
shioyang <shioyang4321@gmail.com>
2017-07-23 15:46:31 +0900

    Show result data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,16 @@ import { ServerService } from './server.service'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app'
   images = [
     {
       name: 'Pred #1',
-      file_name: '1_pred.jpg',
-      path: './ml/data_prediction/1_pred.jpg'
+      file_name: '2_pred.jpg',
+      path: './ml/data_prediction/2_pred.jpg'
     },
     {
       name: 'Pred #2',
-      file_name: '2_pred.jpg',
-      path: './ml/data_prediction/2_pred.jpg'
+      file_name: '1_pred.jpg',
+      path: './ml/data_prediction/1_pred.jpg'
     },
     {
       name: 'Pred #3',
@@ -25,11 +24,15 @@ export class AppComponent {
       path: './ml/data_prediction/3_pred.jpg'
     }
   ]
+
   selected_image = ''
   result_text = ''
+  result_female = 0
+  result_male = 0
   isShowSpinner = false
   isShowFemale = false
   isShowMale = false
+
 
   constructor(private serverService: ServerService){}
 
@@ -44,7 +47,6 @@ export class AppComponent {
     this.serverService.predictImage(image_path)
       .subscribe(result => {
         this.isShowSpinner = false
-        this.result_text = result
 
         if(this.isFemale(result)){
           this.isShowSpinner = false
@@ -56,6 +58,10 @@ export class AppComponent {
           this.isShowFemale = false
           this.isShowMale = true
         }
+
+        this.result_text = result
+        this.result_female = result.female.toFixed(1)
+        this.result_male   = result.male.toFixed(1)
       })
   }
 
