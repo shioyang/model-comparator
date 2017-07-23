65bbbbf64d9abb19b32b4facd83611625863d16e
shioyang <shioyang4321@gmail.com>
2017-07-23 12:41:15 +0900

    Revise predict.py

--- src/ml/predict.py
+++ src/ml/predict.py
@@ -12,6 +12,8 @@ from keras.preprocessing.image import load_img, img_to_array
 img_rows, img_cols = 128, 128
 model_path = 'trained_model.h5'
 
+script_abs_path = os.path.abspath(os.path.dirname(__file__))
+
 
 def get_args():
     parser = argparse.ArgumentParser(description='Predict gender which the person\'s in the image is.')
@@ -19,39 +21,81 @@ def get_args():
     return parser.parse_args()
 
 
-args = get_args()
-img_path = args.input
-
-script_abs_path = os.path.abspath(os.path.dirname(__file__))
-
-
-### Load Image for Prediction
-print('')
-print('### Loading img:', img_path)
-img_full_path = script_abs_path + '/' + img_path
-img = load_img(img_full_path, target_size=(img_rows, img_cols))
-img_arr = img_to_array(img)
-img_array = np.array([img_arr])
-img_array = img_array.astype('float32')
-img_array /= 255
-
-### Load Trained Model
-print('')
-print('### Loading model:', model_path)
-model_full_path = script_abs_path + '/' + model_path
-model = load_model(model_full_path)
-model.summary()
+def load_image(img_full_path):
+    img = load_img(img_full_path, target_size=(img_rows, img_cols))
+    img_arr = img_to_array(img)
+    img_array = np.array([img_arr])
+    img_array = img_array.astype('float32')
+    img_array /= 255
+    return img_array
 
 
-### Prediction
-print('')
-print('### Prediction')
-print('********************')
-print('*   0 for female   *')
-print('*   1 for male     *')
-print('********************')
-results = model.predict(img_array, batch_size=img_array.shape[0])
-for res in results:
-    print('Result:')
-    print('   Female:', res[0] * 100)
-    print('   Male  :', res[1] * 100)
+def load_trained_model(model_full_path):
+    return load_model(model_full_path)
+
+
+def predict(file_path):
+    ### Load Image for Prediction
+    print('')
+    print('### Loading img:', file_path)
+    img_full_path = script_abs_path + '/' + file_path
+    img_array = load_image(img_full_path)
+
+    ### Load Trained Model
+    print('')
+    print('### Loading model:', model_path)
+    model_full_path = script_abs_path + '/' + model_path
+    model = load_trained_model(model_full_path)
+    # model.summary()
+
+    ### Prediction
+    print('')
+    print('### Prediction')
+    print('********************')
+    print('*   0 for female   *')
+    print('*   1 for male     *')
+    print('********************')
+    results = model.predict(img_array, batch_size=img_array.shape[0])
+    result_json = '{}'
+    for res in results:
+        print('Result:')
+        print('   Female:', res[0] * 100)
+        print('   Male  :', res[1] * 100)
+        result_json = '{{ "female": {0}, "male": {1} }}'.format(res[0] * 100, res[1] * 100)
+
+    return result_json
+
+
+if(__name__ == '__main__'):
+    args = get_args()
+    img_path = args.input
+
+    res = predict(img_path)
+    print('res:', res)
+    exit()
+
+    ### Load Image for Prediction
+    print('')
+    print('### Loading img:', img_path)
+    img_full_path = script_abs_path + '/' + img_path
+    img_array = load_image(img_full_path)
+
+    ### Load Trained Model
+    print('')
+    print('### Loading model:', model_path)
+    model_full_path = script_abs_path + '/' + model_path
+    model = load_trained_model(model_full_path)
+    model.summary()
+
+    ### Prediction
+    print('')
+    print('### Prediction')
+    print('********************')
+    print('*   0 for female   *')
+    print('*   1 for male     *')
+    print('********************')
+    results = model.predict(img_array, batch_size=img_array.shape[0])
+    for res in results:
+        print('Result:')
+        print('   Female:', res[0] * 100)
+        print('   Male  :', res[1] * 100)
